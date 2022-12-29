01706f25dc9a9a31e5ea8903de29e1d9b261a9b8
KaterynaKucherenko <estorskaya@gmail.com>
2022-12-29 20:55:33 +0300

    fix3

--- src/main/java/com/epam/mjc/stage0/ArrayTasks.java
+++ src/main/java/com/epam/mjc/stage0/ArrayTasks.java
@@ -64,7 +64,7 @@ public class ArrayTasks {
            int a = 0;
 	    for (int i=0; i< arr.length; i++ ){
 	        if (number == arr[i]){
-	               x = i+1;}
+	               x = i;}
 	             else{ a++;
 	            }
 	            if (a==arr.length){
